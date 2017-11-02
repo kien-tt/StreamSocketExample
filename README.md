0709fa702c2f2792cd0b3af4d1f956473cad0556
Kien Tran Trung <ttki3n@gmail.com>
2017-11-02 12:03:06 +0700

    - Initialize visual studio project. - Hello world!

---
+++ README.md
@@ -0,0 +1,13 @@
+# INTRODUCTION
+
+	This is basic network programming tutorials series for beginners.
+	
+	Using:
+	- Client-Server model
+	- Stream socket (TCP/IP)
+	- Winsock2 library
+	- Microsoft visual studio community 2015
+
+
+
+# EXTERNALS
---
+++ src/EntryPoint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Utils\Logger.h"
+
+int main(int argc, char** argv)
+{
+	LOG_INFO("Starting session ... ... ... ... ... \n");
+
+	LOG_INFO("Hello world ^^! \n");
+
+	LOG_INFO("End session ... ... ... ... ... \n");
+
+	int a;
+	std::cin >> a;
+
+}
